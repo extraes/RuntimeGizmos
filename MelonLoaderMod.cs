fb5b3fd834e701e036e8a8c4f811654be5ffa4e6
extraes <52384576+extraes@users.noreply.github.com>
2021-11-12 02:20:26 -0800

    Initial commit

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,95 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gizmos
+{
+    public static class BuildInfo
+    {
+        public const string Name = "RuntimeGizmos"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = null; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class RuntimeGizmos : MelonMod
+    {
+        private GameObject lineRendererObject;
+        public static LineRenderer lineRenderer;
+
+        public override void OnApplicationStart()
+        {
+            //MelonLogger.Msg("Loading AssetBundle...");
+            var category = MelonPreferences.CreateCategory("RuntimeGizmos");
+            var entry = category.CreateEntry("patchRaycastsAndOverlaps", false, "patchRaycastsAndOverlaps");
+            if (entry.Value) HarmonyInstance.Patch(
+                                typeof(Physics).GetMethod("Raycast",
+                                    new Type[] { typeof(Vector3), typeof(Vector3), typeof(float) }),
+                                postfix: new HarmonyMethod(typeof(RaycastPatch).GetMethod("Postfix"))
+                                );
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            lineRendererObject = new GameObject();
+            lineRenderer = lineRendererObject.AddComponent<LineRenderer>();
+            lineRenderer.material = Resources.FindObjectsOfTypeAll<Material>().FirstOrDefault(m => m.name == "ZPEFM_lighting");
+        }
+    }
+
+    public static class RaycastPatch
+    {
+        public static void Postfix(Vector3 origin, Vector3 direction, float maxDistance)
+        {
+            MelonLogger.Msg("Postfix fired with values:");
+            MelonLogger.Msg($"Origin: {origin.x}, {origin.y}, {origin.z}");
+            MelonLogger.Msg($"Direction: {direction.x}, {direction.y}, {direction.z}");
+            MelonLogger.Msg($"Distance: {maxDistance}");
+            RuntimeGizmos.lineRenderer.SetPositions(new Vector3[]
+            {
+                origin,
+                origin + direction.normalized * maxDistance
+            });
+        }
+
+    }
+
+    public static class Draw
+    {
+        public static void Ray(Ray ray, Color color)
+        {
+
+        }
+
+        public static void Ray(Ray ray, Color color, float distance)
+        {
+
+        }
+
+        public static void Ray(Vector3 start, Vector3 end, Color color)
+        {
+
+        }
+
+    }
+
+    public class GizmoDrawer
+    {
+        static List<Sphere> spheres = new List<Sphere>();
+    }
+
+    struct Sphere
+    {
+
+    }
+
+    struct Line
+    {
+
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(Gizmos.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Gizmos.BuildInfo.Company)]
+[assembly: AssemblyProduct(Gizmos.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Gizmos.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Gizmos.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(Gizmos.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Gizmos.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(Gizmos.RuntimeGizmos), Gizmos.BuildInfo.Name, Gizmos.BuildInfo.Version, Gizmos.BuildInfo.Author, Gizmos.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
